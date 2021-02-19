c202a4fb217e05ea5c6de19bc48a719cfc28d0e0
Terrell Cooper <TerrellCoop@gmail.com>
2021-02-19 12:56:00 -0700

    added in cards projects still need pictures

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -16,7 +16,7 @@ function Cards() {
               path='/projects'
             />
             <CardItem
-              src='images/img-2.jpg'
+              // src='images/img-2.jpg'
               text='ReClip - This app is Snapchat clone with a little twist'
               label='Project'
               path='/projects'
@@ -24,19 +24,19 @@ function Cards() {
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
+              // src='images/img-3.jpg'
               text='Weather App - Pull the weather from any with simple search'
               label='Project'
               path='/project'
             />
             <CardItem
-              src='images/img-4.jpg'
+              // src='images/img-4.jpg'
               text='A show case of my experince and skill'
               label='Resume'
               path='/resume'
             />
             <CardItem
-              src='images/img-8.jpg'
+              // src='images/img-8.jpg'
               text='Brick Breaker - Ready to play classic game of Brick Breaker'
               label='Project'
               path='/project'
